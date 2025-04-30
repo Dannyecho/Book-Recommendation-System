3fa929b074c2eac93a60ea19d63133d93882a4ae
Dannyecho <danielinaju@gmail.com>
2025-04-30 09:38:03 +0100

    Setup app dependencies and UI

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("#smartwizard").smartWizard({
+    selected: 0, // Initial selected step, 0 = first step
+    theme: "dots", // theme for the wizard, related css need to include for other than default theme
+    justified: true, // Nav menu justification. true/false
+    autoAdjustHeight: true, // Automatically adjust content height
+    backButtonSupport: true, // Enable the back button support
+    enableUrlHash: true,
+  });
+});

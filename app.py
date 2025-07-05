3fef2631626d1d49371db4458ec5e8c357c083d5
Dannyecho <danielinaju@gmail.com>
2025-07-05 21:54:45 +0100

    Added fields to get the emotional state

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('form.html')
 
 if __name__ in "__main__":
     app.run(debug = True)
